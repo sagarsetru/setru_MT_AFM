9bd5083efac3aef30d5a5a213f873f4643177351
sagarsetru <sagarsetru@gmail.com>
2019-12-14 03:38:44 -0500

    added axes line width and latex interpreter

--- simplePlotFormat.m
+++ simplePlotFormat.m
@@ -1,15 +1,28 @@
-function simplePlotFormat( xAxisTitle, yAxisTitle, xAxisFontSize, yAxisFontSize, TickValueFontSize )
+function simplePlotFormat( xAxisTitle, yAxisTitle, xAxisFontSize, yAxisFontSize, TickValueFontSize, axesLineWidth, doLatex )
 %SimplePlotFormat.m does basic plot formatting
 %    Sagar Setru
 %    24 Sept 2015
 %    
-%    simplePlotFormat( xAxisTitle, yAxisTitle, xAxisFontSize, yAxisFontSize, TickValueFontSize )
+%    simplePlotFormat( xAxisTitle, yAxisTitle, xAxisFontSize, yAxisFontSize, TickValueFontSize, axesLineWidth, doLatex )
 %%
 
-    xlabel(xAxisTitle,'FontSize',xAxisFontSize)
-    ylabel(yAxisTitle,'FontSize',yAxisFontSize)
-    ax = gca;
-    ax.FontSize = TickValueFontSize;
+    if doLatex
+        
+        xlabel(xAxisTitle,'FontSize',xAxisFontSize,'Interpreter','latex')
+        ylabel(yAxisTitle,'FontSize',yAxisFontSize,'Interpreter','latex')
+        ax = gca;
+        ax.FontSize = TickValueFontSize;
+        set(gca,'LineWidth',axesLineWidth,'TickLabelInterpreter','latex')
+        
+    else
+        
+        xlabel(xAxisTitle,'FontSize',xAxisFontSize)
+        ylabel(yAxisTitle,'FontSize',yAxisFontSize)
+        ax = gca;
+        ax.FontSize = TickValueFontSize;
+        set(gca,'LineWidth',axesLineWidth)
+        
+    end
     
 end
 
