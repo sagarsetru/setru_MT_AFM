11604673fd97210c7cdb2288a21cbb33bff757af
sagarsetru <sagarsetru@gmail.com>
2020-03-26 03:32:01 -0400

    fixed error bar units to nm from um

--- plotLmbVsH.m
+++ plotLmbVsH.m
@@ -1,4 +1,4 @@
-function plotLmbVsH(coatingStruct,tpData,tpLw)
+function plotLmbVsH(coatingStruct,tpData,tpLw,ms)
 %plotLmbVsH.m - fxn to do plot from coating data structure
 % Sagar Setru, 2019 10 23
 %%
@@ -8,7 +8,7 @@ lmb = coatingStruct.lmb;
 errFilm = coatingStruct.errFilm;
 stdErrLmb = coatingStruct.stdErrLmb;
 
-errorbar(film,lmb/1e3,stdErrLmb,stdErrLmb,errFilm,errFilm,tpData,'LineWidth',tpLw)
+errorbar(film,lmb,stdErrLmb*1e3,stdErrLmb*1e3,errFilm,errFilm,tpData,'LineWidth',tpLw,'MarkerSize',ms)
 
 end
 
