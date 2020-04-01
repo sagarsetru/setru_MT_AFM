08eed60e532a91eff693b992ef808973069a9a9b
sagarsetru <sagarsetru@gmail.com>
2020-04-01 15:11:42 -0400

    quick script for measuring branch nucleation fractions

---
+++ findBranchDropFraction.m
@@ -0,0 +1,10 @@
+function [mtfrac] = findBranchDropFraction(mt)
+%findBranchDropFraction.m - quick fxn for fraction of drops that make
+%branches in vitro
+% Sagar Setru, 2020 03 29
+%%
+
+mtfrac = mt(1)/mt(2);
+
+end
+
