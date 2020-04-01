b83afe764712c19a63458755ed01cb5f6bb1eb14
sagarsetru <sagarsetru@gmail.com>
2020-04-01 15:13:21 -0400

    tweaked plotting code s.t. error bars can be chosent o be applied for uncoated and initially coated

--- plotIndivAvgSpectra.m
+++ plotIndivAvgSpectra.m
@@ -60,20 +60,44 @@ end
 % only do error bar plot for droplets
 if strcmp(whichPlot,'d')
     
-    errorbar(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end),stdSpectra(:,initFreqPlotInd:end)/sqrt(size(P1s,2)),col,'LineWidth',psp.lw)
-
-    plot(fMax,maxP,colShape_lmb,'MarkerSize',psp.lmbMarkerSize,'LineWidth',psp.lw)
+    if psp.doPsd == 1
+        errorbar(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end).^2,(stdSpectra(:,initFreqPlotInd:end).^2)/sqrt(size(P1s,2)),col,'LineWidth',psp.lw)        
+    else
+        errorbar(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end),stdSpectra(:,initFreqPlotInd:end)/sqrt(size(P1s,2)),col,'LineWidth',psp.lw)
+    end
+    
+    if psp.doPsd == 1
+        plot(fMax,maxP.^2,colShape_lmb,'MarkerSize',psp.lmbMarkerSize,'LineWidth',psp.lw)
+    else
+        plot(fMax,maxP,colShape_lmb,'MarkerSize',psp.lmbMarkerSize,'LineWidth',psp.lw)
+    end
     
     if size(P1s,2) > 1
         if psp.doScatter
-            plot(f(initFreqPlotInd:end),P1s(initFreqPlotInd:end,:),colShape,'MarkerSize',psp.specMarkerSize)
+            if psp.doPsd == 1
+                plot(f(initFreqPlotInd:end),P1s(initFreqPlotInd:end,:).^2,colShape,'MarkerSize',psp.specMarkerSize)
+            else
+                plot(f(initFreqPlotInd:end),P1s(initFreqPlotInd:end,:),colShape,'MarkerSize',psp.specMarkerSize)                
+            end
         end
     end
     
 else
     
-    plot(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end),col,'LineWidth',psp.lw)
-
+    % commented out code is for spectra for init tpx2 and kinesin
+    if psp.doPsd == 1
+%         if psp.doError_u_i
+%             errorbar(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end).^2,stdSpectra(:,initFreqPlotInd:end).^2,col,'LineWidth',psp.lw)        
+%         else
+            plot(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end).^2,col,'LineWidth',psp.lw)        
+%         end
+    else
+%         if psp.doError_u_i
+%             errorbar(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end),stdSpectra(:,initFreqPlotInd:end),col,'LineWidth',psp.lw)        
+%         else
+            plot(f(initFreqPlotInd:end),avgSpectra(:,initFreqPlotInd:end),col,'LineWidth',psp.lw)
+%         end
+    end
 end
 
 
